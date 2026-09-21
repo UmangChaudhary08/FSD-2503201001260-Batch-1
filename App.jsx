483956158af8d0ef8b4b15b1aaebf0fa7cbb18b7
Umang Chaudhary <chaudharyumang8@gmail.com>
2026-09-21 11:09:45 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,21 +1,29 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
 function App() {
-  const [name, setName] = useState("Umang Sidhu");
-  const [age, setAge] = useState(20);
+  const [status, setStatus] = useState(0);
 
   return (
-    <div>
-      <h1>{name}</h1>
-      <h2>{age}</h2>
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Multiple Conditional Rendering</h1>
 
-      <button onClick={() => setName("Amit")}>
-        Change Name
+      <button onClick={() => setStatus(1)}>
+        Show Student
       </button>
 
-      <button onClick={() => setAge(age + 1)}>
-        Increase Age
+      <button onClick={() => setStatus(2)}>
+        Show Teacher
       </button>
+
+      <button onClick={() => setStatus(3)}>
+        Show Admin
+      </button>
+
+      <hr />
+
+      {status === 1 && <h2>Welcome Student</h2>}
+      {status === 2 && <h2>Welcome Teacher</h2>}
+      {status === 3 && <h2>Welcome Admin</h2>}
     </div>
   );
 }
